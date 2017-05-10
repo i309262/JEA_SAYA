cb3332f31878d1a2bcc1a09389529df03736e189
i309262 <sayalaugs@hotmail.com>
2017-05-10 12:05:53 +0200

    dsgfh

--- target/generated-sources/annotations/model/one/Kweet_.java
+++ target/generated-sources/annotations/model/one/Kweet_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Kweet;
 import model.User;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-10T10:39:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-10T12:03:32")
 @StaticMetamodel(Kweet.class)
 public class Kweet_ { 
 
